2449fe11a7d56d900248e1cadd71f2f5d5688678
Thomas S Hatch <thatch45@gmail.com>
2011-03-17 17:01:56 -0600

    Add some setup to data.py

--- butter/kvm/data.py
+++ butter/kvm/data.py
@@ -1,3 +1,20 @@
 '''
 This module is used to gather the live state data used by butter via salt
 '''
+# Import salt client
+import salt.client
+
+class HVStat(object):
+    '''
+    Detects information about the hypervisors
+    '''
+    def __init__(self):
+        self.local = salt.client.LocalClient()
+        self.hypers = self.__hypers()
+
+    def __hypers(self):
+        '''
+        Return a list, suitable for the salt client, of the hypervisors
+        '''
+        data = self.local.cmd('*', 'virt.is_hyper')
+
