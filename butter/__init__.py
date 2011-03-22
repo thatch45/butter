db743b900a4a436df6b3f6634b0a1cc7e42fbd25
Thomas S Hatch <thatch45@gmail.com>
2011-03-22 17:08:48 -0600

    Add the butter package init frame

---
+++ butter/__init__.py
@@ -0,0 +1,15 @@
+'''
+Initialize command line stacks and direct them into the correct submodule
+'''
+import optparse
+
+def __parse_kvm():
+    '''
+    Parse the butter command line for kvm commands
+    '''
+    pass
+
+def run():
+    '''
+    Execute the body of the butter command
+    '''
