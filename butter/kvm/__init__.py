82f733b166eac409003cf97e45adbcd42489559e
Thomas S Hatch <thatch45@gmail.com>
2011-03-23 17:13:31 -0600

    Add butter kvm daemon startup

--- butter/kvm/__init__.py
+++ butter/kvm/__init__.py
@@ -202,3 +202,55 @@ class KVM(object):
 
         return cli
 
+class KVMD(object):
+    '''
+    The butter kvm subsystem daemon
+    '''
+    def __init__(self, conf='/etc/butter/kvm.conf'):
+        self.cli = self.__parse_cli()
+        self.opts = self.__parse(conf)
+
+    def __parse_cli(self):
+        '''
+        Parse the command line options passed to the clay daemon
+        '''
+        parser = optparse.OptionParser()
+        parser.add_option('-f',
+                '--foreground',
+                default=False,
+                action='store_true',
+                dest='foreground',
+                help='Run the clay daemon in the foreground')
+
+        options, args = parser.parse_args()
+
+        return {'foreground': options.foreground}
+
+    def __parse(self, conf):
+        '''
+        Parse the clay deamon configuration file
+        '''
+        opts = {}
+
+        opts['pool'] = '/srv/vm/images/pool'
+        opts['pool_size'] = '5'
+        opts['keep_old'] = '2'
+        opts['interval'] = '5'
+        opts['image_source'] = ''
+        opts['distros'] = 'arch'
+        opts['format'] = 'raw'
+
+        if os.path.isfile(conf):
+            opts.update(yaml.load(open(conf, 'r')))
+
+        return opts
+
+    def clay_daemon(self):
+        '''
+        Starts the clay daemon
+        '''
+        kvmd = butter.kvm.daemon.Daemon(self.opts)
+        if not self.cli['foreground']:
+            butter.utils.daemonize()
+        kvmd.watch_pool()
+
