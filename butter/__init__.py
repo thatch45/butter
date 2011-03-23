88915134eaabdcaefb8abbb0ae36090bc701fae5
Thomas S Hatch <thatch45@gmail.com>
2011-03-23 15:31:49 -0600

    set up initial cli parsing for butter

--- butter/__init__.py
+++ butter/__init__.py
@@ -1,15 +1,39 @@
 '''
-Initialize command line stacks and direct them into the correct submodule
+Initialize command line stacks and direct them into the correct subsystem
 '''
-import optparse
+import sys
+import butter.kvm
 
-def __parse_kvm():
+subsys = [
+        'kvm',]
+
+def __run_kvm():
     '''
     Parse the butter command line for kvm commands
     '''
-    pass
+    kvm = butter.kvm.KVM()
+    kvm.run()
 
 def run():
     '''
     Execute the body of the butter command
     '''
+    dia = '''
+Welcome to the butter toolkit, butter is the objective execution and management
+layer built on top of the Salt communication framework.
+
+To access the butter help system execute a butter subsytem's --help call:
+    # butter kvm --help
+
+The available subsystems are:'''
+    if len(sys.argv) == 1:
+
+        print dia
+        print subsys
+    elif not subsys.count(sys.argv[1]):
+        print dia
+        print subsys
+
+    {
+     'kvm': __run_kvm(),
+        }[sys.argv[1]]()
