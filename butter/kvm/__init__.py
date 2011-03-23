ef229ccaf1d978dfc3035f9456aedeb74120ed95
Thomas S Hatch <thatch45@gmail.com>
2011-03-23 16:50:39 -0600

    set up initial cli parsing for butter kvm

--- butter/kvm/__init__.py
+++ butter/kvm/__init__.py
@@ -40,8 +40,113 @@ class KVM(object):
         Parse the butter command line options
         '''
         parser = optparse.OptionParser()
+
+        parser.add_option('-C',
+                '--create',
+                dest='create',
+                default=False,
+                action='store_true',
+                help='Set the creation flag')
+
+        parser.add_option('-D',
+                '--destroy',
+                dest='destroy',
+                default=False,
+                action='store_true',
+                help='Force quit the named vm')
+
+        parser.add_option('-P',
+                '--purge',
+                dest='purge',
+                default=False,
+                action='store_true',
+                help='Recursively destroy and delete the named vm')
+
+        parser.add_option('-Q',
+                '--query',
+                dest='query',
+                default=False,
+                action='store_true',
+                help='Set the query flag')
+
+        parser.add_option('-M',
+                '--migrate',
+                dest='migrate',
+                default=False,
+                action='store_true',
+                help='Set the migrate flag')
+
+        parser.add_option('-R',
+                '--reset',
+                dest='reset',
+                default=False,
+                action="store_true",
+                help='Hard reset a vm')
+
         parser.add_option('-c',
-                '--config',
+                '--cpus',
+                dest='cpus',
+                type=int,
+                default=1,
+                help='The number of cpus to give the vm - only read for new'\
+                        + 'virtual machines; default = 1')
+
+        parser.add_option('-m',
+                '--mem',
+                '--memory',
+                dest='mem',
+                type=int,
+                default=1024,
+                help='The amount of ram to give the vm in mebibytes - only read'\
+                        + ' for new virtual machines; default = 1024')
+
+        parser.add_option('-d',
+                '--distro',
+                dest='distro',
+                default='arch',
+                help='The name of the operating system to use, butter will detect'\
+                        + ' and use the latest available image; default = arch')
+
+        parser.add_option('-e',
+                '--env',
+                '--environment',
+                dest='env',
+                default='',
+                help='The puppet environment for this virtual machine to'\
+                    + ' attatch to. By default this will be determined by'
+                    + ' the hostname')
+
+        parser.add_option('-a',
+                '--avail',
+                dest='avail',
+                default=False,
+                action='store_true',
+                help='This flag is for the Query command, this will cause'\
+                    + ' the available resources on the hypervisors to be'\
+                    + ' displayed')
+
+        parser.add_option('-f',
+                '--force',
+                dest='force',
+                default=False,
+                action='store_true',
+                help='Bypass any "are you sure" questions, use with caution!')
+
+        parser.add_option('--clear-node',
+                dest='clear_node',
+                default='',
+                help='Specify which hypervisor to migrate all of the virtual'\
+                    + ' machines off of.')
+
+        parser.add_option('--hyper',
+                '--hypervisor',
+                dest='hyper',
+                default='',
+                help='The explicit name of the hypervisor to use, bypass node'\
+                    + ' detection by butter')
+
+
+        parser.add_option('--config',
                 dest='config',
                 default='/etc/butter/kvm.conf')
 
@@ -49,6 +154,21 @@ class KVM(object):
 
         cli = {}
 
+        cli['create'] = options.create
+        cli['destroy'] = options.destroy
+        cli['purge'] = options.purge
+        cli['query'] = options.query
+        cli['migrate'] = options.migrate
+        cli['reset'] = options.reset
+        cli['name'] = options.name
+        cli['distro'] = options.distro
+        cli['root'] = options.root
+        cli['hyper'] = options.hyper
+        cli['cpus'] = options.cpus
+        cli['mem'] = options.mem
+        cli['avail'] = options.avail
+        cli['force'] = options.force
+        cli['clear_node'] = options.clear_node
         cli['config'] = options.config
 
         # Figure out the fqdn
@@ -58,6 +178,27 @@ class KVM(object):
             cli['fqdn'] = host
         else:
             cli['fqdn'] = host + '.' + dom
+        # Figure out the pins
+        if options.pin:
+            disks = []
+            letters = butter.utils.gen_letters()
+            for disk in options.pin.split(':'):
+                comps = disk.split(',')
+                disks.append({'vd' + letters[len(disks) + 1] + '.' + comps[1]),
+                              'size': comps[0],
+                              'format': comps[1],
+                              'filesystem': comps[2]})
+            cli['pin'] = disks
+        else:
+            cli['pin'] = ''
+        # evaluate the environment
+        if options.env:
+            cli['env'] = options.env
+        else:
+            if cli['fqdn'].count('_'):
+                cli['env'] = cli['fqdn'].split('_')[1].split('.')[0]
+            else:
+                cli['env'] = cli['fqdn'].split('.')[1]
 
         return cli
 
