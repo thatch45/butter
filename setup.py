b8283de9110c1bc8f043813dc624fe90a68ca736
Thomas S Hatch <thatch45@gmail.com>
2011-03-31 10:33:16 -0600

    Add setup.py information for kvmd subsystem

--- setup.py
+++ setup.py
@@ -6,16 +6,23 @@ The setup script for butter
 from distutils.core import setup
 
 setup(name='butter',
-      version='0.1.0',
+      version='0.2.0',
       description='High level execution manager',
       author='Thomas S Hatch',
       author_email='thatch45@gmail.com',
       url='https://github.com/thatch45/butter',
-      packages=['butter', 'butter.kvm'],
-      scripts=['scripts/butter',
+      packages=[
+                'butter',
+                'butter.kvm',
+                'butter.kvmd',
+                ],
+      scripts=[
+               'scripts/butter',
               ],
       data_files=[('/etc/butter',
-                    ['conf/kvm',
+                    [
+                     'conf/kvm',
+                     'conf/kvmd',
                     ]),
                  ],
 
