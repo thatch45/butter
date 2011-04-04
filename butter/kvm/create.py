403d499c1e7f0c8e8b10ce6a23580a48cd5b714e
Thomas S Hatch <thatch45@gmail.com>
2011-04-04 15:26:29 -0600

    I am going to take a short break

--- butter/kvm/create.py
+++ butter/kvm/create.py
@@ -275,7 +275,7 @@ class Create(object):
                     [self.opts['fqdn'], True],
                     expr_form='list')
         else:
-            self.local.cmd(self.opts['fqdn'], 'virt.purge', [host, True])
+            self.local.cmd(host, 'virt.purge', [self.opts['fqdn'], True])
         if os.path.isdir(self.instance):
             shutil.rmtree(self.instance)
 
