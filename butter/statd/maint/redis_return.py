1bfe66ee816f04338ec278d0657a05003ff49cc7
Thomas S Hatch <thatch45@gmail.com>
2011-04-19 14:55:08 -0600

    Add file for redis_return maintainer

---
+++ butter/statd/maint/redis_return.py
@@ -0,0 +1,15 @@
+'''
+Dynamic return manager for redis within statd. The primary purpose for
+maintainers is to clean out old data from the returner's data store.
+If a maintainer is not specified for a returner then the cleaning
+is not executed, or it is assumed that another system is cleaning out old data
+'''
+
+import redis
+
+def clean_old(interval):
+    '''
+    Clear out data that is older than the configured interval from the data
+    store
+    '''
+    pass
