ddea7ee4bfc99c668439cf3109500f9b0940fe30
Thomas S Hatch <thatch45@gmail.com>
2011-04-19 23:55:25 -0600

    Fix typo in the parser

--- butter/statd/__init__.py
+++ butter/statd/__init__.py
@@ -26,7 +26,7 @@ class StatD(object):
         '''
         parser = optparse.OptionParser()
         
-        parser.add_option('-d'
+        parser.add_option('-d',
                 '--daemon',
                 dest='daemon',
                 default=False,
