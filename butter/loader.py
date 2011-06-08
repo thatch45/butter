a1fee7db3631abe796d3c188a093e23b95f837bc
Thomas S Hatch <thatch45@gmail.com>
2011-06-07 22:48:31 -0600

    merge the main and data loader interfaces

--- butter/loader.py
+++ butter/loader.py
@@ -5,16 +5,6 @@ The butter helper interface for the salt module loader
 # Import salt libs
 import salt.loader
 
-def statd_maintainers(dirs=[]):
-    '''
-    Returns the maintainer modules
-    '''
-    module_dirs = [
-        os.path.join(distutils.sysconfig.get_python_lib(), 'butter/statd/maint'),
-        ] + dirs
-    load = salt.loader.Loader(module_dirs, opts)
-    return load.filter_func('maintain')
-
 def statd_data(dirs=[]):
     '''
     Return the data backend modules
@@ -23,7 +13,7 @@ def statd_data(dirs=[]):
         os.path.join(distutils.sysconfig.get_python_lib(), 'butter/statd/data'),
         ] + dirs
     load = salt.loader.Loader(module_dirs, opts)
-    return load.filter_func('data')
+    return load.gen_functions()
 
 def statd_alert(dirs=[]):
     '''
