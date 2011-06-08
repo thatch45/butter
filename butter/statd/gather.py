a9096b46ade144f8f2e483cc38cc60b3b97bc9f1
Thomas S Hatch <thatch45@gmail.com>
2011-06-07 20:23:15 -0600

    Remove load_maintainers, making a generic loader interface

--- butter/statd/gather.py
+++ butter/statd/gather.py
@@ -8,6 +8,7 @@ import time
 # Import salt modules
 import salt.client
 # Import butter modules
+import butter.loader
 import butter.statd.maint
 
 # Import cython 
@@ -27,42 +28,6 @@ class Gather(object):
         self.opts = opts
         self.local = salt.client.LocalClient(self.opts['master_config'])
         self.maint = self.__load_maintainers()
-
-    def __load_maintainers(self):
-        '''
-        Read in the maintainer functions
-        '''
-        mods = set()
-        maint = {}
-        maint_dir = os.path.join(distutils.sysconfig.get_python_lib(),
-                'butter/statd/maint')
-        for fn_ in os.listdir(maint_dir):
-            if fn_.startswith('__init__.py'):
-                continue
-            if fn_.endswith('.pyo')\
-                    or fn_.endswith('.py')\
-                    or fn_.endswith('.pyc'):
-                mods.add(fn_[:fn_.rindex('.')])
-            if fn_.endswith('.pyx') and cython_enable:
-                mods.add(fn_[:fn_.rindex('.')])
-
-        for mod in mods:
-            if self.opts['disable_maintainers'].count(mod):
-                continue
-            try:
-                tmodule = __import__('butter.statd.maint', globals(), locals(), [mod])
-                module = getattr(tmodule, mod)
-                if hasattr(module, '__opts__'):
-                    module.__opts__.update(self.opts)
-                else:
-                    module.__opts__ = self.opts
-            except:
-                continue
-            if hasattr(module, 'clean_old'):
-                if callable(module.clean_old):
-                    maint[mod] = module
-
-        return maint
 
     def stat_cmd(self):
         '''
