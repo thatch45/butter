85ced72bf4ab10147370fc1931f0016bf1ab1183
Thomas S Hatch <thatch45@gmail.com>
2011-06-10 15:18:54 -0600

    hook data mining into monitor

--- butter/statd/monitor.py
+++ butter/statd/monitor.py
@@ -3,6 +3,7 @@ Reads the data returned from the gather system and executes any alerts
 '''
 # Import Python Libs
 import datetime
+import sys
 # Import butter libs
 import butter.loader
 
@@ -13,8 +14,23 @@ class Monitor(object):
     def __init__(self, opts):
         self.opts = opts
         self.data = buter.loader.statd_data(self.opts['data_dirs'])
+        self.mine = self.__get_miner()
 
-    def fresh_data():
+    def __get_miner(self):
+        '''
+        Return the function used to mine data
+        '''
+        name = '{0[data_backend]}_data.mine'.format(self.opts)
+        if self.data.has_key(name):
+            return self.data[name]
+        err = 'Data backend for {0[data_backend]} was not found, check the'\
+            + ' "data_backend" configuration value.'
+        err.format(self.opts)
+        sys.stderr.write(err)
+        sys.exit(2)
+
+    def fresh_data(self):
         '''
         Return the data set as it is configured
         '''
+        return self.mine(self.opts['sampling_frame'], self.opts['target'])
