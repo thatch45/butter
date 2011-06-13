6bbc39da92e8f84ff04d752699b5344bb47ae8fb
Thomas S Hatch <thatch45@gmail.com>
2011-06-13 16:00:19 -0600

    Add method to construct commands and arguments for stat gathering

--- butter/statd/gather.py
+++ butter/statd/gather.py
@@ -28,13 +28,34 @@ class Gather(object):
         self.opts = opts
         self.local = salt.client.LocalClient(self.opts['master_config'])
 
+    def construct_arg(self):
+        '''
+        Return the command and argument constructs derived from the
+        configuration, if no configuration is set, butter statd will exit
+        '''
+        if not self.opts['stats']:
+            err = 'No stats configuration has been set, please set up the'\
+                + ' stats value in the butter statd configuration file'
+            sys.stderr.write(err)
+            sys.exit(2)
+        cmds = []
+        args = []
+        for call, data in self.opts['status'].items():
+            cmds.append(call)
+            if data.has_key('arg'):
+                args.append(data['args'])
+            else:
+                args.append('')
+        return cmds, args
+
     def stat_cmd(self):
         '''
         Execute the salt command to inform the minions to return stats
         '''
+        cmds, args = self.construct_cmd()
         cmd = [self.opts['target'],
-               self.opts['cmd'],
-               self.opts['arg'],
+               cmd,
+               arg,
                0,
                self.opts['target_type'],
                self.opts['returner']]
