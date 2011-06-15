df81a59bf3a08f4c04b29c221295626528e58f05
Thomas S Hatch <thatch45@gmail.com>
2011-06-15 10:23:16 -0600

    Fix issue in butter loader

--- butter/loader.py
+++ butter/loader.py
@@ -5,23 +5,23 @@ The butter helper interface for the salt module loader
 # Import salt libs
 import salt.loader
 
-def statd_data(dirs=[], opts):
+def statd_data(opts):
     '''
     Return the data backend modules
     '''
     module_dirs = [
         os.path.join(distutils.sysconfig.get_python_lib(), 'butter/statd/data'),
-        ] + dirs
+        ] + opts['data_dirs']
     load = salt.loader.Loader(module_dirs, opts)
     return load.gen_functions(opts)
 
-def statd_alert(dirs=[], opts):
+def statd_alert(opts):
     '''
     Return the alerter modules
     '''
     module_dirs = [
         os.path.join(distutils.sysconfig.get_python_lib(), 'butter/statd/alert'),
-        ] + dirs
+    ] + opts['alert_dirs']
     load = salt.loader.Loader(module_dirs, opts)
     return load.filter_func('alert')
 
